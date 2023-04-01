c314b08849b88e53d60daf67a2ce67f2de237df1
Lucasi17 <112277023+Lucasi17@users.noreply.github.com>
2023-04-01 14:36:42 -0300

    F#015

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import "../about/About.css";
 
-const About = () => {
-    return (
-        <div>
-            <h1>Sobre Nosotros</h1>
-        </div>
-    );
-};
+function About() {
+  return (
+    <div className="about-container bg-transparent text-dark" >
+      <h4>Nuestra Historia</h4>
+      <p>
+        Rolling Surveys es una plataforma de encuestas líder
+        en el mundo en servicios de encuestas web y
+        generación de datos estadisticos ayudando a miles de
+        empresas, personas y organizaciones en más de 150
+        países a desbloquear el conocimiento de los
+        consumidores.
+        <br />
+        Rolling Surveys es conocido por su experiencia en
+        soluciones de data web y sus trabajos de primer
+        nivel. Nuestra pericia y basta experiencia han hecho
+        que las expectativas de nuestros clientes sean
+        ampliamente superadas.
+      </p>
+
+      <h1>Nuestra Mision</h1>
+      <p>
+        Nuestra misión es alcanzar el maximo de nuestras
+        capacidades, para generar una web de encuestas fácil
+        de usar, brindando a nuestros usuarios de la mejor
+        información para sus tomas de decisiones.
+      </p>
+
+      <h1>Nuestra Visión</h1>
+      <p>
+        Sembrar las semillas de los servicios por excelencia
+        con un enfoque centrado en el cliente y cosechar la
+        confianza de clientes en todo el mundo.
+      </p>
+    </div>
+  );
+}
 
 export default About;
