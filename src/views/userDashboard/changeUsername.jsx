1ead6366d30ae34c9f00a53a0c8b8c9af8c75bb3
Francisco Iturburu <f.iturburu@outlook.com>
2023-04-01 00:37:16 -0300

    Fix: Add disable attribute to show password on changeUsername component

--- src/views/userDashboard/changeUsername.jsx
+++ src/views/userDashboard/changeUsername.jsx
@@ -97,7 +97,7 @@ const ChangeUsernameComponent = ({URL,token}) =>{
           type={showPassword? 'text': 'password'}
           aria-label="password"
         />
-        <Button  variant="outline-primary" onClick={()=> setShowPassword(!showPassword)}> {showPassword? <i class="bi bi-eye-slash"></i> : <i class="bi bi-eye"></i>}  </Button>
+        <Button disabled={disabled} variant="outline-primary" onClick={()=> setShowPassword(!showPassword)}> {showPassword? <i class="bi bi-eye-slash"></i> : <i class="bi bi-eye"></i>}  </Button>
       </InputGroup>
       <div className='mt-2 mb-2'>
         {errorMessage}
